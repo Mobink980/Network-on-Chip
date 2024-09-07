f63637c39ba57821f2e9e43237d9574250518763
Ali Karazmoodeh <mobink980@gmail.com>
2024-09-07 13:22:47 +0330

    Update Bus.cc
    Change broadcast of bus to multi-packet operation of router

--- src/mem/ruby/network/onyx/Bus.cc
+++ src/mem/ruby/network/onyx/Bus.cc
@@ -57,7 +57,8 @@ Bus::Bus(const Params &p)
   : BasicBus(p), Consumer(this), m_latency(p.latency),
     m_virtual_networks(p.virt_nets), m_vc_per_vnet(p.vcs_per_vnet),
     m_num_vcs(m_virtual_networks * m_vc_per_vnet), m_bit_width(p.width),
-    m_network_ptr(nullptr), switchAllocator(this), crossbarSwitch(this)
+    m_network_ptr(nullptr), routingUnit(this), switchAllocator(this),
+    crossbarSwitch(this)
 {
     m_input_unit.clear(); //clear the inports
     m_output_unit.clear(); //clear the outports
@@ -78,14 +79,14 @@ Bus::init()
 //Loop through all OutputUnits and call their wakeup()
 //Call SwitchAllocator's wakeup()
 //Call CrossbarSwitch's wakeup()
-//The bus's wakeup function is called whenever any of its modules
+//The router's wakeup function is called whenever any of its modules
 //(InputUnit, OutputUnit, SwitchAllocator, CrossbarSwitch) have a
 //ready flit/credit to act upon this cycle.
 void
 Bus::wakeup()
 {
-    DPRINTF(RubyNetwork, "Bus %d woke up\n", m_id);
-    //ensure the bus woke up on a clockEdge (the tick when a cycle begins)
+    DPRINTF(RubyNetwork, "Router %d woke up\n", m_id);
+    //ensure the router woke up on a clockEdge (the tick when a cycle begins)
     assert(clockEdge() == curTick());
 
     // check for incoming flits (wake up all the inports)
@@ -110,14 +111,23 @@ Bus::wakeup()
     crossbarSwitch.wakeup();
 }
 
-//The following function of the Bus class adds one input port or
-//inport to the Bus object.
+//The following function of the Router class adds one input port or
+//inport to the Router object.
 void
 Bus::addInPort(PortDirection inport_dirn,
                   NetLink *in_link, AckLink *credit_link)
 {
+    //======================================================
+    // std::cout << "=================================================\n";
+    // std::cout << "name of the link (from addInPort in Bus.cc): " << in_link->name() <<"\n";
+    // std::cout << "width of the link (from addInPort in Bus.cc): " << in_link->bitWidth <<"\n";
+    // std::cout << "id of the router (from addInPort in Bus.cc): " << m_id <<"\n";
+    // std::cout << "width of the router (from addInPort in Bus.cc): " << m_bit_width <<"\n";
+    // std::cout << "=================================================\n";
+    //======================================================
+
     fatal_if(in_link->bitWidth != m_bit_width, "Widths of link %s(%d)does"
-            " not match that of Bus%d(%d). Consider inserting SerDes "
+            " not match that of Router%d(%d). Consider inserting SerDes "
             "Units.", in_link->name(), in_link->bitWidth, m_id, m_bit_width);
 
     //port number of this inport
@@ -125,16 +135,16 @@ Bus::addInPort(PortDirection inport_dirn,
     //the port number of our new inport
     int port_num = m_input_unit.size();
 
-    //Defining an input port for this bus object.
-    //This refers to an object of the Bus class in InputUnit
+    //Defining an input port for this router object.
+    //This refers to an object of the Router class in InputUnit
     //class instantiation. inport_dirn is the direction of the
     //input port that we are creating.
     BusInport *input_unit = new BusInport(port_num, inport_dirn, this);
-    //set network link for this inport (to this bus)
+    //set network link for this inport (to this router)
     input_unit->set_in_link(in_link);
-    //set credit link for this inport (from this bus to the adjacent one)
+    //set credit link for this inport (from this router to the adjacent one)
     input_unit->set_credit_link(credit_link);
-    //the consumer of the network link is this bus
+    //the consumer of the network link is this router
     in_link->setLinkConsumer(this);
     //set the number of virtual channels per virtual network for the network link
     in_link->setVcsPerVnet(get_vc_per_vnet());
@@ -142,18 +152,28 @@ Bus::addInPort(PortDirection inport_dirn,
     credit_link->setSourceQueue(input_unit->getCreditQueue(), this);
     //set the number of virtual channels per virtual network for the credit link
     credit_link->setVcsPerVnet(get_vc_per_vnet());
-    //add the input port we created to the bus object
+    //add the input port we created to the router object
     m_input_unit.push_back(std::shared_ptr<BusInport>(input_unit));
+    //add the new inport and its direction to the routingUnit
+    routingUnit.addInDirection(inport_dirn, port_num);
 }
 
-//The following function of the Bus class adds one output port or
-//outport to the Bus object.
+//The following function of the Router class adds one output port or
+//outport to the Router object.
 void
 Bus::addOutPort(PortDirection outport_dirn,
                    NetLink *out_link,
                    std::vector<NetDest>& routing_table_entry, int link_weight,
                    AckLink *credit_link, uint32_t consumerVcs)
 {
+    //======================================================
+    // std::cout << "=================================================\n";
+    // std::cout << "name of the link (from addOutPort in Bus.cc): " << out_link->name() <<"\n";
+    // std::cout << "width of the link (from addOutPort in Bus.cc): " << out_link->bitWidth <<"\n";
+    // std::cout << "id of the router (from addOutPort in Bus.cc): " << m_id <<"\n";
+    // std::cout << "width of the router (from addOutPort in Bus.cc): " << m_bit_width <<"\n";
+    // std::cout << "=================================================\n";
+    //======================================================
     fatal_if(out_link->bitWidth != m_bit_width, "Widths of units do not match."
             " Consider inserting SerDes Units");
 
@@ -162,17 +182,17 @@ Bus::addOutPort(PortDirection outport_dirn,
     //the port number of our new outport
     int port_num = m_output_unit.size();
 
-    //Defining an output port for this bus object.
+    //Defining an output port for this router object.
     //outport_dirn is the direction of the output port that we
     //are creating. consumerVcs is the virtual channels that
     //would consume from each outport.
     BusOutport *output_unit = new BusOutport(port_num, outport_dirn, this,
                                              consumerVcs);
-    //set network link for this outport (from this bus to the adjacent one)
+    //set network link for this outport (from this router to the adjacent one)
     output_unit->set_out_link(out_link);
-    //set credit link for this outport (to this bus)
+    //set credit link for this outport (to this router)
     output_unit->set_credit_link(credit_link);
-    //the consumer of the credit link is this bus
+    //the consumer of the credit link is this router
     credit_link->setLinkConsumer(this);
     //set the number of virtual channels per virtual network for the credit link
     //These virtual channels are consumerVcs.
@@ -181,25 +201,42 @@ Bus::addOutPort(PortDirection outport_dirn,
     out_link->setSourceQueue(output_unit->getOutQueue(), this);
     //set the number of virtual channels per virtual network for the network link
     out_link->setVcsPerVnet(consumerVcs);
-    //add the output port we created to the bus object
+    //add the output port we created to the router object
     m_output_unit.push_back(std::shared_ptr<BusOutport>(output_unit));
+
+    //add the route of the routing_table_entry to the routingUnit
+    routingUnit.addRoute(routing_table_entry);
+    //add the weight of the link to the routingUnit
+    //This is for the network link, and since a link connects one
+    //outport to an inport, by giving weight while creating an outport,
+    //we cover the total links in the network.
+    routingUnit.addWeight(link_weight);
+    //add the new outport and its direction to the routingUnit
+    routingUnit.addOutDirection(outport_dirn, port_num);
 }
 
-//Getting the direction of an outport in the bus
+//Getting the direction of an outport in the router
 PortDirection
 Bus::getOutportDirection(int outport)
 {
     return m_output_unit[outport]->get_direction();
 }
 
-//Getting the direction of an inport in the bus
+//Getting the direction of an inport in the router
 PortDirection
 Bus::getInportDirection(int inport)
 {
     return m_input_unit[inport]->get_direction();
 }
 
-//===================================================================
+//The following function of the Router class computes which outport should
+//be chosen for the flits, based on route, inport, and inport direction.
+int
+Bus::route_compute(RouteInfo route, int inport, PortDirection inport_dirn)
+{
+    return routingUnit.outportCompute(route, inport, inport_dirn);
+}
+
 //This function grants the switch to an inport, so the flit could pass
 //the crossbar.
 void
@@ -207,14 +244,27 @@ Bus::grant_switch(int inport, chunk *t_flit)
 {
     crossbarSwitch.update_sw_winner(inport, t_flit);
 }
-//===================================================================
 
-//This function gives the bus, time cycles delay.
+//This function gives the router, time cycles delay.
 void
 Bus::schedule_wakeup(Cycles time)
 {
     // wake up after time cycles
     scheduleEvent(time);
+}
+
+//get the layer of a router based on its id
+int
+Bus::get_router_layer(int router_id) {
+    int num_rows = m_network_ptr->getNumRows();
+    int num_cols = m_network_ptr->getNumCols();
+    int num_layers = m_network_ptr->getNumLayers();
+    assert(num_rows > 0 && num_cols > 0 && num_layers > 0);
+    //number of routers or RLIs per layer
+    int num_routers_layer = num_rows * num_cols;
+    if (num_layers > 1) { return floor(router_id/num_routers_layer); }
+    //return 0 if we only have one layer
+    return 0;
 }
 
 //Getting the direction of a port as a string
@@ -229,7 +279,7 @@ Bus::getPortDirectionName(PortDirection direction)
     return direction;
 }
 
-//This function is for creating statistics for every bus
+//This function is for creating statistics for every router
 //in the stats.txt file.
 void
 Bus::regStats()
@@ -263,7 +313,7 @@ Bus::regStats()
     ;
 }
 
-//This function collates the stats for the bus.
+//This function collates the stats for the router.
 void
 Bus::collateStats()
 {
@@ -300,7 +350,7 @@ Bus::printFaultVector(std::ostream& out)
     int num_fault_types = m_network_ptr->fault_model->number_of_fault_types;
     float fault_vector[num_fault_types];
     get_fault_vector(temperature_celcius, fault_vector);
-    out << "Bus-" << m_id << " fault vector: " << std::endl;
+    out << "Router-" << m_id << " fault vector: " << std::endl;
     for (int fault_type_index = 0; fault_type_index < num_fault_types;
          fault_type_index++) {
         out << " - probability of (";
@@ -319,7 +369,7 @@ Bus::printAggregateFaultProbability(std::ostream& out)
     float aggregate_fault_prob;
     get_aggregate_fault_probability(temperature_celcius,
                                     &aggregate_fault_prob);
-    out << "Bus-" << m_id << " fault probability: ";
+    out << "Router-" << m_id << " fault probability: ";
     out << aggregate_fault_prob << std::endl;
 }
 
