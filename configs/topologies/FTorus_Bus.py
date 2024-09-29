737467e61578b1747ed0ab30d26a91724fefc03e
Ali Karazmoodeh <mobink980@gmail.com>
2024-09-29 08:00:18 +0330

    Update FTorus_Bus.py
    A topology that combines Bus and Folded Torus architecture

--- configs/topologies/FTorus_Bus.py
+++ configs/topologies/FTorus_Bus.py
@@ -50,29 +50,29 @@ class FTorus_Bus(SimpleTopology):
         num_rows = options.mesh_rows  # number of rows in 3D mesh
         num_columns = options.mesh_columns  # number of columns in 3D mesh
         num_layers = options.mesh_layers  # number of layers in 3D mesh
-        # ******************************************************
-        # Adding some extra middle routers (16 for 64 main routers)
-        # These extra routers connect routers in different layers
+        num_routers_layer = num_rows * num_columns
+        #********************************************************************
+        # For every 4 routers, we have a bus connecting them across layers
         num_routers = options.num_cpus
         num_busses = num_rows * num_columns
-        # ******************************************************
+        #********************************************************************
 
         # default values for link latency and router latency.
         # Can be over-ridden on a per link/router basis
         link_latency = options.link_latency  # used by simple and garnet
         bus_link_latency = 0  # latency of bus links
         router_latency = options.router_latency  # only used by garnet
-        # ==============================================================
+        #============================================================
         bus_latency = 1
-        # ==============================================================
+        #============================================================
 
         # There must be an evenly divisible number of cntrls to routers
         # Also, obviously the number or rows must be <= the number of routers
-        # ******************************************************
+        #****************************************************************
         cntrls_per_router, remainder = divmod(len(nodes), num_routers)
         assert num_rows > 0 and num_rows <= num_routers
         assert num_rows * num_columns * num_layers == num_routers
-        # ******************************************************
+        #****************************************************************
 
         # Create the routers in the mesh
         routers = [
@@ -81,20 +81,17 @@ class FTorus_Bus(SimpleTopology):
         ]
         network.routers = routers  # Add the routers to the network
 
-        # &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+        #&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
         # Create the busses
         busses = [
-            Bus(bus_id=i, latency=bus_latency) for i in range(num_busses)
+            Bus(bus_id=i, latency=bus_latency) 
+            for i in range(num_busses)
         ]
         network.busses = busses  # Add the created busses to the network
-        # &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+        #&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 
         # link counter to set unique link ids
         link_count = 0
-        # ========================================
-        bus_to_router_count = 0
-        router_to_bus_count = 0
-        # ========================================
 
         # Add all but the remainder nodes to the list of nodes to be uniformly
         # distributed across the network.
@@ -140,13 +137,28 @@ class FTorus_Bus(SimpleTopology):
 
         network.ext_links = ext_links  # Add the external links to the network
 
-        # Create the 3D mesh links.
-        int_links = []
-        # ====================================
-        bus_to_router_links = []
-        router_to_bus_links = []
-        # ====================================
+        #====================================================================
+        #====================================================================
+        # Connect each node to the appropriate bus
+        bus_links = []
+        for i in range(num_routers_layer):
+            for j in range(num_layers):
+                bus_links.append(
+                    BusLink(
+                        link_id=link_count,
+                        ext_node=network_nodes[i + (j * num_routers_layer)],
+                        int_node=busses[i],
+                        latency=bus_link_latency
+                    )
+                )
+                link_count += 1
 
+        network.bus_links = bus_links  # Add the bus links to the network
+        #====================================================================
+        #====================================================================
+
+        # Create the 2D mesh links for each layer
+        int_links = []
         # connect routers in each layer with internal links and
         # also update link_count to its latest value
         for layer in range(num_layers):
@@ -161,29 +173,8 @@ class FTorus_Bus(SimpleTopology):
                 routers,
             )
 
-        # connect routers in different layers via Bus
-        self.connectInterLayerLinks(
-            num_layers,
-            num_rows,
-            num_columns,
-            BusToRouterLink,
-            RouterToBusLink,
-            bus_to_router_links,
-            router_to_bus_links,
-            bus_to_router_count,
-            router_to_bus_count,
-            link_latency,
-            routers,
-            busses,
-        )
-
         network.int_links = int_links  # Add the internal links to the network
-        # ================================================================
-        # Add the bus to router links to the network
-        network.bus_to_router_links = bus_to_router_links
-        # Add the router to bus links to the network
-        network.router_to_bus_links = router_to_bus_links
-        # ================================================================
+
 
     # For connecting the routers via internal links
     # in each layer.
@@ -201,26 +192,35 @@ class FTorus_Bus(SimpleTopology):
         # number of routers in one layer
         num_routers_layer = num_rows * num_columns
 
-        # East outport to West inport links (weight = 1)
-        # It means link from east output port of the left router
-        # to the west input port of the right router.
-        for row in range(num_rows):
-            for col in range(num_columns):
-                if col + 1 < num_columns:
-                    # id of the left router
-                    east_out = col + (row * num_columns)
-                    # id of the right router
-                    west_in = (col + 1) + (row * num_columns)
+        for i in range(num_rows):  # for horizontal links (East and West)
+            start_node = i * num_columns
+            end_node = start_node + num_columns - 1
+            ftorus_list = []
+
+            ftorus_list.append(start_node)
+            for j in range(start_node + 1, end_node + 1, 2):
+                ftorus_list.append(j)
+
+            for j in range(end_node - 1, start_node - 1, -2):
+                ftorus_list.append(j)
+
+            # remove the last element if there are only two columns
+            if num_columns == 2:
+                ftorus_list.pop()
+
+            for k in range(len(ftorus_list) - 1):
+                if k % 2 == 0:
                     int_links.append(
                         IntLink(
                             link_id=link_count,
                             src_node=routers[
-                                east_out + (layer * num_routers_layer)
+                                ftorus_list[k] + (layer * num_routers_layer)
                             ],
                             dst_node=routers[
-                                west_in + (layer * num_routers_layer)
+                                ftorus_list[k + 1]
+                                + (layer * num_routers_layer)
                             ],
-                            src_outport="East",
+                            src_outport="West",
                             dst_inport="West",
                             latency=link_latency,
                             weight=1,
@@ -228,26 +228,36 @@ class FTorus_Bus(SimpleTopology):
                     )
                     link_count += 1
 
-        # West outport to East inport links (weight = 1)
-        # It means link from west output port of the right router
-        # to the east input port of the left router.
-        for row in range(num_rows):
-            for col in range(num_columns):
-                if col + 1 < num_columns:
-                    # id of the left router
-                    east_in = col + (row * num_columns)
-                    # id of the right router
-                    west_out = (col + 1) + (row * num_columns)
                     int_links.append(
                         IntLink(
                             link_id=link_count,
                             src_node=routers[
-                                west_out + (layer * num_routers_layer)
+                                ftorus_list[k + 1]
+                                + (layer * num_routers_layer)
                             ],
                             dst_node=routers[
-                                east_in + (layer * num_routers_layer)
+                                ftorus_list[k] + (layer * num_routers_layer)
                             ],
                             src_outport="West",
+                            dst_inport="West",
+                            latency=link_latency,
+                            weight=1,
+                        )
+                    )
+                    link_count += 1
+
+                else:
+                    int_links.append(
+                        IntLink(
+                            link_id=link_count,
+                            src_node=routers[
+                                ftorus_list[k] + (layer * num_routers_layer)
+                            ],
+                            dst_node=routers[
+                                ftorus_list[k + 1]
+                                + (layer * num_routers_layer)
+                            ],
+                            src_outport="East",
                             dst_inport="East",
                             latency=link_latency,
                             weight=1,
@@ -255,137 +265,121 @@ class FTorus_Bus(SimpleTopology):
                     )
                     link_count += 1
 
-        # North outport to South inport links (weight = 2)
-        # It means link from north output port of the down router
-        # to the south input port of the up router.
-        for col in range(num_columns):
-            for row in range(num_rows):
-                if row + 1 < num_rows:
-                    # id of the up router
-                    south_in = col + ((row + 1) * num_columns)
-                    # id of the down router
-                    north_out = col + (row * num_columns)
                     int_links.append(
                         IntLink(
                             link_id=link_count,
                             src_node=routers[
-                                north_out + (layer * num_routers_layer)
+                                ftorus_list[k + 1]
+                                + (layer * num_routers_layer)
                             ],
                             dst_node=routers[
-                                south_in + (layer * num_routers_layer)
+                                ftorus_list[k] + (layer * num_routers_layer)
                             ],
-                            src_outport="North",
-                            dst_inport="South",
+                            src_outport="East",
+                            dst_inport="East",
                             latency=link_latency,
-                            weight=2,
+                            weight=1,
                         )
                     )
                     link_count += 1
 
-        # South outport to North inport links (weight = 2)
-        # It means link from south output port of the up router
-        # to the north input port of the down router.
-        for col in range(num_columns):
-            for row in range(num_rows):
-                if row + 1 < num_rows:
-                    # id of the up router
-                    south_out = col + ((row + 1) * num_columns)
-                    # id of the down router
-                    north_in = col + (row * num_columns)
+        for i in range(num_columns):  # for vertical links (North and South)
+            start_node = i
+            end_node = start_node + (num_rows - 1) * num_columns
+            ftorus_list = []
+
+            ftorus_list.append(start_node)
+            for j in range(
+                start_node + num_columns, end_node + 1, 2 * num_columns
+            ):
+                ftorus_list.append(j)
+
+            for j in range(
+                end_node - num_columns, start_node - 1, (-2) * num_columns
+            ):
+                ftorus_list.append(j)
+
+            # remove the last element if there are only two rows
+            if num_rows == 2:
+                ftorus_list.pop()
+
+            for k in range(len(ftorus_list) - 1):
+                if k % 2 == 0:
                     int_links.append(
                         IntLink(
                             link_id=link_count,
                             src_node=routers[
-                                south_out + (layer * num_routers_layer)
+                                ftorus_list[k] + (layer * num_routers_layer)
                             ],
                             dst_node=routers[
-                                north_in + (layer * num_routers_layer)
+                                ftorus_list[k + 1]
+                                + (layer * num_routers_layer)
                             ],
-                            src_outport="South",
+                            src_outport="North",
                             dst_inport="North",
                             latency=link_latency,
-                            weight=2,
+                            weight=1,
+                        )
+                    )
+                    link_count += 1
+
+                    int_links.append(
+                        IntLink(
+                            link_id=link_count,
+                            src_node=routers[
+                                ftorus_list[k + 1]
+                                + (layer * num_routers_layer)
+                            ],
+                            dst_node=routers[
+                                ftorus_list[k] + (layer * num_routers_layer)
+                            ],
+                            src_outport="North",
+                            dst_inport="North",
+                            latency=link_latency,
+                            weight=1,
+                        )
+                    )
+                    link_count += 1
+
+                else:
+                    int_links.append(
+                        IntLink(
+                            link_id=link_count,
+                            src_node=routers[
+                                ftorus_list[k] + (layer * num_routers_layer)
+                            ],
+                            dst_node=routers[
+                                ftorus_list[k + 1]
+                                + (layer * num_routers_layer)
+                            ],
+                            src_outport="South",
+                            dst_inport="South",
+                            latency=link_latency,
+                            weight=1,
+                        )
+                    )
+                    link_count += 1
+
+                    int_links.append(
+                        IntLink(
+                            link_id=link_count,
+                            src_node=routers[
+                                ftorus_list[k + 1]
+                                + (layer * num_routers_layer)
+                            ],
+                            dst_node=routers[
+                                ftorus_list[k] + (layer * num_routers_layer)
+                            ],
+                            src_outport="South",
+                            dst_inport="South",
+                            latency=link_latency,
+                            weight=1,
                         )
                     )
                     link_count += 1
 
         return link_count
 
-    # For connecting two routers via an IntLink
-    def connect_routers(
-        self,
-        bus_to_router_links,
-        router_to_bus_links,
-        BusToRouterLink,
-        RouterToBusLink,
-        link_latency,
-        bus_to_router_count,
-        router_to_bus_count,
-        layer_number,
-        router,
-        bus,
-    ):
-        # Add the bus to router link from bus to router
-        bus_to_router_links.append(
-            BusToRouterLink(
-                link_id=bus_to_router_count,
-                src_node=bus,
-                dst_node=router,
-                src_outport="Down" + str(layer_number),
-                dst_inport="Up",
-                latency=0,
-                weight=3,
-            )
-        )
-
-        # Add the router to bus link from router to bus
-        router_to_bus_links.append(
-            RouterToBusLink(
-                link_id=router_to_bus_count,
-                src_node=router,
-                dst_node=bus,
-                src_outport="Up",
-                dst_inport="Down" + str(layer_number),
-                latency=0,
-                weight=3,
-            )
-        )
-
-    # For connecting routers in different layers via Bus.
-    def connectInterLayerLinks(
-        self,
-        num_layers,
-        num_rows,
-        num_columns,
-        BusToRouterLink,
-        RouterToBusLink,
-        bus_to_router_links,
-        router_to_bus_links,
-        bus_to_router_count,
-        router_to_bus_count,
-        link_latency,
-        routers,
-        busses,
-    ):
-        # number of routers in one layer
-        num_routers_layer = num_rows * num_columns
-
-        for i in range(num_routers_layer):
-            for j in range(num_layers):
-                self.connect_routers(
-                    bus_to_router_links,
-                    router_to_bus_links,
-                    BusToRouterLink,
-                    RouterToBusLink,
-                    link_latency,
-                    bus_to_router_count,
-                    router_to_bus_count,
-                    j,
-                    routers[i + (j * num_routers_layer)],
-                    busses[i],
-                )
-                bus_to_router_count += 1
-                router_to_bus_count += 1
 
     # Register nodes with filesystem
     def registerTopology(self, options):
